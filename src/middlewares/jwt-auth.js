cf919c06dbbf6d40400e1456287109b91610b846
Lucas Santos <lhs.santoss@gmail.com>
2022-05-26 18:52:42 -0300

    feat: add express jwt
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/middlewares/jwt-auth.js
+++ src/middlewares/jwt-auth.js
@@ -1,24 +1,11 @@
-const jwt = require('jsonwebtoken')
 const { jwt: jwtConfig } = require('../config')
-const { AuthenticationError } = require('../errors')
-
-const jwtAuth = async (req, res, next) => {
-  const header = req.get('Authorization')
-  if (!header) throw new AuthenticationError('No token provided')
-  try {
-    const token = header.split(' ')[1]
-    const decoded = jwt.verify(token, jwtConfig.secret, {
-      audience: jwtConfig.audience,
-      issuer: jwtConfig.issuer,
-    })
-    req.user = decoded
-    next()
-  } catch (error) {
-    if (error instanceof jwt.JsonWebTokenError) next(new AuthenticationError('Invalid token'))
-    next(error)
-  }
-}
+const { expressjwt } = require('express-jwt')
 
 module.exports = {
-  jwtAuth
+  jwtAuth: expressjwt({
+    secret: jwtConfig.secret,
+    audience: jwtConfig.audience,
+    issuer: jwtConfig.issuer,
+    algorithms: ['HS256'],
+  }),
 }
